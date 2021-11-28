60c3052f9ba326e3131e28be0cf1de8f54dbf930
ambagape <ambagape@gmail.com>
2021-11-28 12:58:30 +0100

    update and add .gitignore

--- src/lib/ngx-amb-video-recorder.service.spec.ts
+++ src/lib/ngx-amb-video-recorder.service.spec.ts
@@ -1,16 +1,38 @@
 import { TestBed } from '@angular/core/testing';
-
-import { NgxAmbVideoRecorderService } from './ngx-amb-video-recorder.service';
+import { INgxAmbVideoRecorderService } from './ngx-amb-video-recorder.service';
 
 describe('NgxAmbVideoRecorderService', () => {
-  let service: NgxAmbVideoRecorderService;
+  let service: INgxAmbVideoRecorderService;
+  let mediaStreamMock: jasmine.SpyObj<MediaStream>;
 
   beforeEach(() => {
+    mediaStreamMock = jasmine.createSpyObj('MediaStream',['ondataavailable','getTracks','start','onstop','onerror','state','stop']);
     TestBed.configureTestingModule({});
-    service = TestBed.inject(NgxAmbVideoRecorderService);
+    service = TestBed.inject(INgxAmbVideoRecorderService);
+
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
+
+  it('should start recording', async () => {
+    const recorder = await service.startRecording(mediaStreamMock,5000);
+    expect(recorder).toBeTruthy();
+  });
+
+  it('should stop recording and get last recorded video-url', async () => {
+    await service.startRecording(mediaStreamMock,5000);
+    const tracks: Array<jasmine.SpyObj<MediaStreamTrack>> = [];
+    for(let i = 0; i < 1; i++){
+      const track: jasmine.SpyObj<MediaStreamTrack> = jasmine.createSpyObj('MediaStreamTrack',['stop']);
+      tracks.push(track);
+    }
+    mediaStreamMock.getTracks.and.returnValues(tracks);
+    await service.stopRecording(mediaStreamMock);
+    expect(tracks[0].stop).toHaveBeenCalled();
+    const blob = await service.getLastRecordedClip();
+    expect(blob).toBeTruthy();
+  });
+
 });
--- src/lib/ngx-amb-video-recorder.service.ts
+++ src/lib/ngx-amb-video-recorder.service.ts
@@ -1,9 +1,60 @@
 import { Injectable } from '@angular/core';
 
-@Injectable({
-  providedIn: 'root'
-})
-export class NgxAmbVideoRecorderService {
+
+class NgxAmbVideoRecorderService implements INgxAmbVideoRecorderService {
 
   constructor() { }
+
+  async startRecording(stream: MediaStream, durationInMS: number): Promise<Array<Blob>> {
+    let recorder = new MediaRecorder(stream);
+    let data = [];
+
+    recorder.ondataavailable = event => data.push(event.data);
+    recorder.start();
+
+    let stopped = new Promise((resolve, reject) => {
+      recorder.onstop = resolve;
+      recorder.onerror = event => reject(event);
+    });
+
+    let recorded = this.wait(durationInMS).then(
+      () => recorder.state == "recording" && recorder.stop()
+    );
+
+    await Promise.all([
+      stopped,
+      recorded
+    ]);
+    return data;
+  }
+
+  stopRecording(stream: MediaStream): void {
+    stream.getTracks().forEach(track => track.stop());
+  }
+
+  getLastRecordedClip(): Promise<Blob> {
+    throw new Error('Method not implemented.');
+  }
+
+  private wait(delayInMS: number): Promise<any> {
+    return new Promise(resolve => setTimeout(resolve, delayInMS));
+  }
+
 }
+
+
+@Injectable({
+  providedIn: 'root',
+  useClass: NgxAmbVideoRecorderService
+})
+export abstract class INgxAmbVideoRecorderService {
+
+  abstract startRecording(stream: MediaStream, durationInMS: number): Promise<Array<Blob>>;
+
+  abstract stopRecording(stream: MediaStream): void;
+
+  abstract getLastRecordedClip(): Promise<Blob>;
+
+}
+
+
